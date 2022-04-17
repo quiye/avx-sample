25271e1cec7d442ebc26ef1f66da40a38267e6b2
Ishida Yuya <quiye@users.noreply.github.com>
2022-04-17 10:58:36 +0000

    fix

--- avx.cc
+++ avx.cc
@@ -51,34 +51,53 @@ float f2(const std::vector<float>& l, decltype(l) r) {
 float f3(const std::vector<float>& li, decltype(li) ri) {
   using packedFloats = std::array<float, siz256>;
   std::vector<packedFloats> l, r;
-  for (size_t i = 0; i < li.size() && i < ri.size(); i += 8) {
-    l.push_back(vec2arr(li.data() + i));
-    r.push_back(vec2arr(ri.data() + i));
+  for (size_t i = 0; i < li.size() && i < ri.size(); i += siz256) {
+    l.push_back(vec2arr(&li[i]));
+    r.push_back(vec2arr(&ri[i]));
   }
 
   __m256 base = _mm256_setzero_ps();
   for (size_t i = 0; i < l.size(); i++) {
-    base = _mm256_fmadd_ps(_mm256_load_ps(l[i].data()), _mm256_load_ps(r[i].data()), base);
+    auto p1 = _mm256_loadu_ps(l[i].data());  // l[i]はメモリ領域の開始がalignedされてない可能性があるのでloadu
+    auto p2 = _mm256_loadu_ps(r[i].data());
+    base = _mm256_fmadd_ps(p1, p2, base);
   }
+
   std::array<float, siz256> s;
   _mm256_store_ps(s.data(), base);
   return std::accumulate(s.cbegin(), std::next(s.cbegin(), siz256), float{});
 }
 
+float f4(const std::vector<float>& l, decltype(l) r) {
+  __m256 base = _mm256_setzero_ps();
+  const auto limit = std::min(l.size(), r.size());
+  size_t i = 0;
+  for (; i + siz256 - 1 < limit; i += siz256) {
+    base = _mm256_fmadd_ps(_mm256_loadu_ps(&l[i]), _mm256_loadu_ps(&r[i]), base);
+  }
+  std::array<float, siz256> s;
+  _mm256_storeu_ps(s.data(), base);
+  return std::accumulate(s.cbegin(), s.cend(), float{})  // 本体
+         + std::inner_product(std::next(l.cbegin(), i),
+                              std::next(l.cbegin(), limit),
+                              std::next(r.cbegin(), i),
+                              float{});  // 余り
+}
+
 int main() {
   using namespace std;
   vector<float> v;
-  for (size_t i = 0; i < 100; i++) {
+  for (size_t i = 0; i < 35; i++) {
     v.push_back(static_cast<float>(i) / 11);
   }
   vector<float> u;
-  for (size_t i = 0; i < 64; i++) {
-    u.push_back(static_cast<float>(i) / 11);
+  for (size_t i = 0; i < 3123; i++) {
+    u.push_back(static_cast<float>(i) / 13);
   }
   const auto len = std::min(v.size(), u.size());
-  const auto resp = std::inner_product(v.cbegin(), std::next(v.cbegin() + len), u.cbegin(), float{0});
-
+  const auto resp = std::inner_product(v.cbegin(), std::next(v.cbegin(), len), u.cbegin(), float{0});
   std::cout << resp << std::endl;
   std::cout << f2(v, u) << std::endl;
   std::cout << f3(v, u) << std::endl;
+  std::cout << f4(v, u) << std::endl;
 }
